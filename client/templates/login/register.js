27ad11b6a781561f836ed2d651f7e4c3d75a742d
wangsen <wangs@tongxinyiliao.com>
2015-05-23 13:14:29 +0800

    accounts-phone package add in local

--- client/templates/login/register.js
+++ client/templates/login/register.js
@@ -1,20 +1,32 @@
-//Template.register.events({
-//    'submit form': function(e) {
-//        e.preventDefault();
-//
-//        var $target = $(e.target);
-//
-//        // TODO: get phone
-//        var phone = $target.find('[name=phone]').val();
-//        // TODO: get password
-//        var password = $target.find('[name=password]').val();
-//        // TODO: get password_again
-//        var password_again = $target.find('[name=password_again]').val();
-//
-//        if(password !== password_again){
-//            alert('两次密码输入不一致');
-//        }
-//
-//    }
-//
-//});
+Template.register.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var $target = $(e.target);
+
+        // TODO: get phone
+        var phone = $target.find('[name=phone]').val();
+        if(!phone.match('^1\\d{10}$')){
+            alert("输入手机号不合法");
+            return ;
+        }
+        // TODO: get password
+        var password = $target.find('[name=password]').val();
+        // TODO: get password_again
+        var password_again = $target.find('[name=password_again]').val();
+
+        if(password !== password_again){
+            alert('两次密码输入不一致');
+            return ;
+        }
+
+        Accounts.createUserWithPhone({phone:phone, password:password}, function (error,userId){
+            if(error){
+                alert(error);
+            }else{
+                alert(userId);
+            }
+        });
+    }
+
+});
